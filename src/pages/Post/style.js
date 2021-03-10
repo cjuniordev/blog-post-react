e7df33ad5654ffcd0bfb14c860659fc5e1e1bf7b
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-10 20:39:45 -0300

    Defining background and spaces on the page

--- src/pages/Post/style.js
+++ src/pages/Post/style.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
 
-  padding: 0 55px;
+  padding: 15px 55px;
 
   display: flex;
   flex-direction: row;
   align-item: center;
   justify-content: space-between;
+
+  background-color: #EEF0F1;
 `;
