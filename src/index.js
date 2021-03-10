224b3f71ce016bd73a2f787bd80217f5062fd7fb
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-10 17:09:09 -0300

    add reset css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/reset.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
