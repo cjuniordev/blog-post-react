1c4a99a12a1abf3489513ba3bee1a722b1a65ca4
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-11 18:00:46 -0300

    Add ProfilePhoto on Aside Bar

--- src/pages/Post/AsideBar/index.js
+++ src/pages/Post/AsideBar/index.js
@@ -1,8 +1,16 @@
-import { Container } from './style';
+import { Container, Profile, Trending, Banner, ProfilePhoto } from './style';
+import Photo from '../../../components/ProfilePhoto';
 
 function AsideBar() {
   return (
     <Container>
+      <Profile>
+        <Banner />
+        <ProfilePhoto>
+          <Photo/>
+        </ProfilePhoto>
+      </Profile>
+      <Trending></Trending>
     </Container>
   );
 }
--- src/pages/Post/AsideBar/style.js
+++ src/pages/Post/AsideBar/style.js
@@ -4,5 +4,31 @@ export const Container = styled.div`
   height: 100vh;
   width: 30%;
 
+    border-radius: 6px 6px 0 0;
   background-color: #c1ffc1;
+`;
+
+export const Profile = styled.div`
+  width: 100%;
+`;
+
+export const Banner = styled.div`
+  height: 30px;
+  width: 100%;
+
+  border-radius: 6px 6px 0 0;
+  background-color: #000000;
+`;
+
+export const Trending = styled.div`
+  width: 100%;
+`;
+
+export const ProfilePhoto = styled.div`
+  height: 30px;
+  width: 100%;
+
+  position: relative;
+  top: -15px;
+  left: 15px;
 `;
