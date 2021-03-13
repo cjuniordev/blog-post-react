01762cb9bdd5837dc15485300d40f140e2521dcb
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-13 13:39:18 -0300

    Add Cover to Main component

--- src/pages/Post/Main/index.js
+++ src/pages/Post/Main/index.js
@@ -1,8 +1,11 @@
-import { Container } from './style';
+import { Container, Cover, Img } from './style';
 
 function Main() {
   return (
     <Container>
+      <Cover>
+        <Img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" alt="Cover"/>
+      </Cover>
     </Container>
   );
 }
--- src/pages/Post/Main/style.js
+++ src/pages/Post/Main/style.js
@@ -3,6 +3,17 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
   width: 62%;
+`;
 
-  background-color: #000000;
+export const Cover = styled.div`
+  height: 300px;
+  width: 100%;
+`;
+
+export const Img = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+
+  border-radius: 10px 10px 0 0;
 `;
