c700f7fca4eda0b604a60ec769f50e2a19cc3d01
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-13 14:03:19 -0300

    Add Title and Tags of the Post

--- src/pages/Post/Main/index.js
+++ src/pages/Post/Main/index.js
@@ -1,4 +1,4 @@
-import { Container, Cover, Img } from './style';
+import { Container, Cover, Img, Content, Header, Title, Tags, Tag } from './style';
 
 function Main() {
   return (
@@ -6,6 +6,16 @@ function Main() {
       <Cover>
         <Img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" alt="Cover"/>
       </Cover>
+      <Content>
+        <Header>
+          <Title>Lorem Ipsum Dolor Sit Amet Consectetur Adipiscing Elit.</Title>
+          <Tags>
+            <Tag bgcolor={'#F7DF1E'} color={'#000000'}>javascript</Tag>
+            <Tag bgcolor={'#3D8836'} color={'#FFF'}>nodejs</Tag>
+            <Tag bgcolor={'#562765'} color={'#FFF'}>reactjs</Tag>
+          </Tags>
+        </Header>
+      </Content>
     </Container>
   );
 }
--- src/pages/Post/Main/style.js
+++ src/pages/Post/Main/style.js
@@ -17,3 +17,44 @@ export const Img = styled.img`
 
   border-radius: 10px 10px 0 0;
 `;
+
+export const Content = styled.div`
+  padding: 32px 64px 0;
+
+  background-color: #FFF;
+`;
+
+export const Header = styled.div`
+
+`;
+
+export const Title = styled.h1`
+ font-size: 48px;
+ font-weight: 800;
+ line-height: 60px;
+ margin-bottom: 16px;
+`;
+
+
+export const Tags = styled.div`
+  margin-bottom: 16px;
+`;
+
+export const Tag = styled.span`
+  &:before{
+    content: '#';
+    color: #000000;
+    opacity: 0.4;
+  }
+
+  padding: 4px;
+  margin-right: 4px;
+  border-radius: 5px;
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18.2px;
+
+  color: ${props => props.color};
+  background-color: ${props => props.bgcolor};
+`;
