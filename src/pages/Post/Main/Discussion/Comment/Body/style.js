b67036806faebec1c3759d45c34d9427d0e5a86c
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-16 17:00:57 -0300

    Fix Overflow size

--- src/pages/Post/Main/Discussion/Comment/Body/style.js
+++ src/pages/Post/Main/Discussion/Comment/Body/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  box-sizing: border-box;
   width: 100%;
   padding: 0 12px;
 `;
@@ -8,6 +9,8 @@ export const Container = styled.div`
 export const Paragraph = styled.p`
   font-size: 18px;
   line-height: 27px;
-  
+
   margin-bottom: 20px;
+
+  overflow-wrap: break-word;
 `;
